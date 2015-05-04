e74a1d30250b42716427b56213e7b2f6aea00294
eternnoir <eternnoir@gmail.com>
2015-05-04 13:33:02 +0800

    static done.

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"path/filepath"
+	"strconv"
+)
+
+var (
+	port = flag.Int("port", 8080, "Which port to listen")
+	path = flag.String("path", "./", "The root path where want to serve.")
+)
+
+func main() {
+	flag.Parse()
+	rootPath, err := filepath.Abs(*path)
+	if err != nil {
+		log.Fatal("Path Error: %v", err)
+	}
+	log.Printf("HTTP Static File Server start at http://%s:%d", "localhost", *port)
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*port), http.FileServer(http.Dir(rootPath))))
+}
